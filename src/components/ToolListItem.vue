011e6afc58d932d293e1111781ed41e320120670
gabrielblimoni <gabrielblimoni@gmail.com>
2020-08-19 00:18:30 -0300

    initial setup + basic UI (bootstrap-vue)

---
+++ src/components/ToolListItem.vue
@@ -0,0 +1,48 @@
+<template>
+    <b-card text-variant="dark">
+        <template v-slot:header>
+            <a :href="url" target="blank">
+                <h5 class="mb-0">{{ name }}</h5>
+            </a>
+        </template>
+
+        <p v-html="description"></p>
+
+        <b-button 
+            variant="primary" 
+            class="mr-1"
+            @click="handleEditToolClick"
+        >
+            <b-icon-pencil></b-icon-pencil>
+        </b-button>
+        <b-button 
+            variant="danger"
+            @click="handleDeleteToolClick"
+        >
+            <b-icon-trash></b-icon-trash>
+        </b-button>
+    </b-card>
+</template>
+
+<script>
+export default {
+    props: {
+        id: { type: String, description: 'Tool ID', required: true },
+        name: { type: String, description: 'Tool name', required: true },
+        description: { type: String, description: 'Tool description' },
+        url: { type: String, description: 'Tool URL' },
+    },
+    methods: {
+        handleEditToolClick () {
+            console.log('edit tool')
+        },
+        handleDeleteToolClick () {
+            console.log('delete tool')
+        },
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="home">
+    <b-container>
+      <b-card bg-variant="dark" text-variant="white" style="min-height: 400px;">
+        <template v-slot:header>
+          <b-row>
+            <b-col>
+              <h4 class="mb-0">Lista de ferramentas</h4>
+            </b-col>
+            <b-col>
+              <b-button 
+                variant="primary"
+                @click="handleAddNewToolClick"
+              >
+                Adicionar
+                <b-icon-plus></b-icon-plus>
+              </b-button>
+            </b-col>
+          </b-row>
+        </template>
+
+        <template>
+          <b-row>
+            <b-col sm="12" md="6" class="mb-3">
+              <tool-list-item 
+                name="Ferramenta Doida"
+                description="descrição ... descrição ... descrição ... descrição ... descrição ... descrição ... descrição ... descrição ... descrição ... descrição ... "
+                id="k1j23i12i3ji210239821uijn"
+                url="//google.com"
+              />
+            </b-col>
+          </b-row>
+        </template>
+
+        <template v-slot:footer>
+          <h5 class="mb-0">Lista de ferramentas</h5>
+        </template>
+      </b-card>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import ToolListItem from '../components/ToolListItem'
+
+export default {
+  name: 'Home',
+  components: {
+    ToolListItem,
+  },
+  methods: {
+    handleAddNewToolClick () {
+      console.log('Create New Tool')
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.home {
+  padding: 20px 0;
+}
+</style>
