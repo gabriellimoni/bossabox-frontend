b809bb0f7db39484c3ea5b785bb9c452200f8f52
gabrielblimoni <gabrielblimoni@gmail.com>
2020-08-22 19:53:52 -0300

    clearing app

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,11 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  async created () {
-    // Just to hold user logged in while there is no user handling on frontend
-    await this.$store.dispatch('user/login', {
-      email: 'gabrielblimoni@gmail.com',
-      password: 'gabriel123'
-    })
-    setInterval(async () => {
-      await this.$store.dispatch('token/refreshToken')
-    }, 1 * 1000 * 30)
-    // endGambi
-  }
 }
 </script>
 
