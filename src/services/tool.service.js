b408b54b899f250c4fd855b3274493e761795a73
gabrielblimoni <gabrielblimoni@gmail.com>
2020-08-22 14:50:17 -0300

    create tool service

--- src/services/tool.service.js
+++ src/services/tool.service.js
@@ -1,9 +1,19 @@
 import api from './api.service'
+import errorList from '../config/errorList'
 
 export async function getTools () {
     const { data: toolsData } = await api.get('tools').catch(error => {
-        // handle error
+        // Todo - handle error
         throw 'Error listing'
     })
     return toolsData
+}
+
+export async function createTool (tool) {
+    const { data } = await api.post('tools', tool).catch(error => {
+        // Todo - handle error
+        throw errorList.CREATE_TOOL_ERROR
+    })
+
+    return data
 }
