7a36aba7c3359c32ff751ff565870f52966e0546
gabrielblimoni <gabrielblimoni@gmail.com>
2020-08-22 14:50:57 -0300

    toolForm component

---
+++ src/components/ToolForm.vue
@@ -0,0 +1,112 @@
+<template>
+    <div text-variant="dark">
+        <b-form>
+            <b-form-group
+                label="Nome:"
+            >
+                <b-form-input
+                    type="text"
+                    required
+                    placeholder="Nome da ferramenta"
+                    @change="setToolTitle($event)"
+                ></b-form-input>
+            </b-form-group>
+            <b-form-group
+                label="Link:"
+            >
+                <b-form-input
+                    type="text"
+                    required
+                    placeholder="https://..."
+                    @change="setToolLink($event)"
+                ></b-form-input>
+            </b-form-group>
+            <b-form-group
+                label="Descrição:"
+            >
+                <b-form-textarea
+                    required
+                    rows="3"
+                    placeholder="Descrição da ferramenta"
+                    @change="setToolDescription($event)"
+                ></b-form-textarea>
+            </b-form-group>
+
+            <label>Tags:</label>
+            <b-form-tags 
+                v-model="tags" 
+                class="mb-4"
+                placeholder="Adicione uma tag..."
+                addButtonText="Adicionar"
+            >
+            </b-form-tags>
+
+            <div style="text-align: right;">
+                <b-button 
+                    variant="outline-success" 
+                    outlined
+                    class="mr-1"
+                    @click="saveTool"
+                >
+                    Salvar
+                </b-button>
+                <b-button 
+                    variant="outline-danger"
+                    @click="close"
+                >
+                    Cancelar
+                </b-button>
+            </div>
+        </b-form>
+    </div>
+</template>
+
+<script>
+import { createNamespacedHelpers } from 'vuex'
+const { mapState, mapMutations, mapActions } = createNamespacedHelpers('toolForm')
+export default {
+    methods: {
+        async saveTool () {
+            if (this.mode === 'creating') {
+                await this.createTool()
+            } else if (this.mode === 'editing') {
+                
+            }
+            this.close()
+        },
+        close () {
+            console.log('a')
+            this.$emit('close')
+        },
+        ...mapMutations([
+            'setToolTitle',
+            'setToolLink',
+            'setToolDescription',
+            'setToolTags',
+        ]),
+        ...mapActions([
+            'createTool',
+        ]),
+    },
+    computed: {
+        tags: {
+            get () {
+                return this.$store.state.toolForm.tags
+            },
+            set (tags) {
+                this.setToolTags(tags)
+            }
+        },
+        ...mapState([
+            '_id',
+            'title',
+            'link',
+            'description',
+            'mode',
+        ])
+    },
+}
+</script>
+
+<style scoped>
+</style>
