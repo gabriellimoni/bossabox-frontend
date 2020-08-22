d71e98f3ad06fadfa0de90bca5893d5ade575783
gabrielblimoni <gabrielblimoni@gmail.com>
2020-08-22 13:48:28 -0300

    refresh token user service

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -17,3 +17,19 @@ export async function loginByEmailAndPassword (email, password) {
     setAuthToken(loginData.token)
     return loginData
 }
+
+export async function refreshToken (refreshToken) {
+    const { data: refreshData } = await api.post('refresh', {
+        refresh_token: refreshToken,
+    }).catch(error => {
+        if (error && error.response && error.response.data) {
+            const errorData = error.response.data
+            if (errorData.code === 'auth/not_authorized') throw errorList.USER_NOT_AUTHORIZED
+            if (errorData.code === 'user/not_found') throw errorList.USER_NOT_FOUND
+        }
+        throw errorList.UNKNOW_ERROR
+    })
+
+    setAuthToken(refreshData)
+    return refreshData
+}
