0348862a0aeeeea2413123b9ef131b03bf2e920f
gabrielblimoni <gabrielblimoni@gmail.com>
2020-08-22 13:49:07 -0300

    api set auth token fix

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -4,8 +4,8 @@ const api = axios.create({
     baseURL: process.env.VUE_APP_API_BASE_URL,
 })
 
-export async function setAuthToken (token) {
-    api.defaults.headers['x-access-token'] = token
+export async function setAuthToken (tokenData) {
+    api.defaults.headers['x-access-token'] = tokenData.accessToken
 }
 
 export default api
