54a1030a95d3d56fdb3cbb6429e3eac1028fc834
gabrielblimoni <gabrielblimoni@gmail.com>
2020-08-22 20:44:05 -0300

    refactor to error service

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,17 +1,12 @@
 import api, { setAuthToken } from './api.service'
-import errorList from '../config/errorList'
+import { handleError } from './error.service'
 
 export async function loginByEmailAndPassword (email, password) {
     const { data: loginData } = await api.post('login', {
         email,
         password,
     }).catch(error => {
-        if (error && error.response && error.response.data) {
-            const errorData = error.response.data
-            if (errorData.code === 'auth/not_authorized') throw errorList.USER_NOT_AUTHORIZED
-            if (errorData.code === 'user/not_found') throw errorList.USER_NOT_FOUND
-        }
-        throw errorList.UNKNOW_ERROR
+        return handleError(error)
     })
 
     setAuthToken(loginData.token)
@@ -22,12 +17,7 @@ export async function refreshToken (refreshToken) {
     const { data: refreshData } = await api.post('refresh', {
         refresh_token: refreshToken,
     }).catch(error => {
-        if (error && error.response && error.response.data) {
-            const errorData = error.response.data
-            if (errorData.code === 'auth/not_authorized') throw errorList.USER_NOT_AUTHORIZED
-            if (errorData.code === 'user/not_found') throw errorList.USER_NOT_FOUND
-        }
-        throw errorList.UNKNOW_ERROR
+        return handleError(error)
     })
 
     setAuthToken(refreshData)
